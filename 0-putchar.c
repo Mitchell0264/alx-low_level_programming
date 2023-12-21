899a32540191e4eefb54355b72316dff7bf4ab04
Mitchell0264 <ayovuatamitchell@gmail.com>
2023-12-20 16:46:19 -0800

    _putchar

--- 0-putchar.c
+++ 0-putchar.c
@@ -1,10 +1,18 @@
-#include <unistd.h>
+#include "main.h"
 /**
  * main - A program that prints _putchar, followed by a new line.
  * Return: 0 (success).
  */
-int main(void)
+int main()
 {
-	write(1, "_putchar\n", 9);
-	return 0;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
