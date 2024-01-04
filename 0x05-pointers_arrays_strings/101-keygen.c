e6ff5759ef86b5f1e4fc9cb8ce7a0cbed6ba21c3
Mitchell0264 <ayovuatamitchell@gmail.com>
2024-01-03 19:45:52 -0800

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void generatePassword(char *password, int length) 
+{
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	int i;
+
+	srand((unsigned int)time(NULL));
+	for (i = 0; i < length - 1; ++i)
+	{
+		password[i] = charset[rand() % (sizeof(charset) - 1)];
+	}
+	password[length - 1] = '\0';
+}
+
+int main()
+{
+	int passwordLength = 12;
+	char password[passwordLength];
+
+	generatePassword(password, passwordLength);
+	printf("Generated Password: %s\n", password);
+	return 0;
+}
